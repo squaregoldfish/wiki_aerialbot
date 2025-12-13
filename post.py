acdd70150dc73ec6af08e936cdcd504a89e0d88b
Steve Jones <steve@squaregoldfish.co.uk>
2025-12-13 13:24:54 +0100

    Choose a random item to post

--- post.py
+++ post.py
@@ -9,7 +9,7 @@ config = toml.load('config.toml')
 with sqlite3.connect(config['database']['file']) as conn:
     cursor = conn.cursor()
 
-    cursor.execute('SELECT id, title, longitude, latitude, ml_width, human_width FROM pages WHERE longitude IS NOT NULL AND (ml_width IS NOT NULL OR human_width IS NOT NULL) AND posted = 0 ORDER BY loaded DESC')
+    cursor.execute('SELECT id, title, longitude, latitude, human_width FROM pages WHERE longitude IS NOT NULL AND human_width IS NOT NULL AND posted = 0 ORDER BY RANDOM()')
     record = cursor.fetchone()
     print(record)
 
@@ -18,9 +18,7 @@ with sqlite3.connect(config['database']['file']) as conn:
     command = ['python', 'aerialbot.py', f'-p={record[3]},{record[2]}', '-t', f'{record[1]}\nhttps://en.wikipedia.org/wiki/{record[0]}\n\n{{point_fancy}}']
 
     width = None
-    if record[5] is not None:
-        width = record[5]
-    elif record[4] is not None:
+    if record[4] is not None:
         width = record[4]
 
     if width is not None:
